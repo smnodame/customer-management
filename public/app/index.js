22e8fbe84d002402710b12c7b6879bb7c43cf417
boonprakit <boonprakit@proteus-tech.com>
2018-06-01 02:30:16 +0700

    create html for add owner bussiness info

--- public/app/index.js
+++ public/app/index.js
@@ -14,6 +14,14 @@ app.run(function($rootScope) {
 app.controller('homeCtrl', [
     '$scope', '$location', '$route', '$rootScope', '$routeParams',
     function($scope, $location, $route, $rootScope, $routeParams) {
+        $scope.step = 1
 
+        $scope.click_next = () => {
+            $scope.step = $scope.step + 1
+        }
+
+        $scope.click_previous = () => {
+            $scope.step = $scope.step - 1
+        }
     }
 ])
