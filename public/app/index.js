839fb9e58c4eb1af4e7b2178f49c282aec886381
boonprakit <boonprakit@proteus-tech.com>
2018-06-02 18:46:37 +0700

    create customersCtrl

--- public/app/index.js
+++ public/app/index.js
@@ -19,21 +19,7 @@ app.controller('customersCtrl', [
     '$scope', '$location', '$route', '$rootScope', '$routeParams',
     function($scope, $location, $route, $rootScope, $routeParams) {
         $(document).ready(function() {
-            $('#datatable').dataTable()
-            $('#datatable-keytable').DataTable({
-                keys: true
-            })
             $('#datatable-responsive').DataTable()
-            $('#datatable-scroller').DataTable({
-            ajax: "js/datatables/json/scroller-demo.json",
-                deferRender: true,
-                scrollY: 380,
-                scrollCollapse: true,
-                scroller: true
-            })
-            var table = $('#datatable-fixed-header').DataTable({
-                fixedHeader: true
-            })
         })
     }
 ])
