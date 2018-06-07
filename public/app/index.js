64f9b962b1b22cde0b5274dffbf31e1717eb992d
boonprakit <boonprakit@proteus-tech.com>
2018-06-08 03:24:34 +0700

    create query for select customer

--- public/app/index.js
+++ public/app/index.js
@@ -95,5 +95,71 @@ app.controller('homeCtrl', [
                 window.scrollTo(0, 0)                
             }
         }
+
+        $scope.detail = {
+            goal_id: '',
+            goal_detail: '',
+            goal_file_name: '',
+
+            business_detail_id: '',
+            business_detail_pet_quantity: '',
+            business_detail_meat_exchange_rate: '',
+            business_detail_sixness_rate: '',
+            business_detail_cpf_product_usage_rate: '',
+            business_detail_other_product_usage_rate: '',
+            business_detail_sales_chanels_for_cpf: '',
+            business_detail_sales_chanels_for_other: '',
+            business_detail_number_of_workers: '',
+            business_detail_competitor: '',
+            business_detail_market_condition_and_solutions: '',
+            business_detail_mortality_rate: '',
+
+            executive_profile_id: '',
+            executive_profile_name: '',
+            executive_profile_age: '',
+            executive_profile_sex: '',
+            executive_profile_education: '',
+            executive_profile_status: '',
+            executive_profile_career: '',
+            executive_profile_experience: '',
+
+            child_profile_name: '',
+            child_profile_age: '',
+            child_profile_sex: '',
+            child_profile_career: 'fsa',
+            child_profile_experience: 'fad',
+            child_profile_education: 'fasd',
+
+            spouse_profile_name: 'fasd',
+            spouse_profile_age: 'fasd',
+            spouse_profile_education: 'fasd',
+            spouse_profile_career: 'fsad',
+            spouse_profile_experience: 'fa',
+
+            
+            financial_information_id: '',
+            financial_information_payment_history: '',
+            financial_information_credit_from_cpf: '',
+            financial_information_credit_from_competitor: '',
+            financial_information_private_capital_rate: '',
+            financial_information_other_capital_rate: '',
+            financial_information_asset_land: '',
+            financial_information_asset_car: '',
+            financial_information_asset_other: '',
+            financial_information_debt: '',
+            financial_information_main_revenue: '',
+            financial_information_other_income: '',
+            financial_information_cpf_feed_purchase: '',
+            financial_information_other_feed_purchase: '',
+            financial_information_breeding_grounds: '',
+            financial_information_price_of_animals: '',
+            financial_information_quantity_of_animals_purchase: '',
+
+            business_id: '',
+            business_name: '',
+            business_address: '',
+            business_type: '',
+            business_telephone: ''
+        }
     }
 ])
--- server.js
+++ server.js
@@ -15,31 +15,110 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  port     : '3306',
-  user     : 'root',
-  password : '',
-  database : 'customer_management_db'
+    host     : '127.0.0.1',
+    port     : '3306',
+    user     : 'root',
+    password : '',
+    database : 'customer_management_db'
 })
 
-const apiRoutes = express.Router()
+const api_routes = express.Router()
 
-apiRoutes.get('/customers', function(req, res) {
-  connection.query('SELECT * FROM `goal`, business_detail, executive_profile, financial_information, main_business where main_business.business_id = financial_information.financial_information_id and main_business.business_id = executive_profile.executive_profile_id and main_business.business_id = business_detail.business_detail_id and main_business.business_id = business_detail.business_detail_id', function (err, rows, fields) {
-    if (err) throw err
-    res.json(rows)
-  })
+const query_service = {
+    account: {
+        insert: function(account_id, account_first_name, account_last_name, account_email, account_phone, account_password, account_photo_path, account_address) {
+            return "INSERT INTO `account` (`account_id`, `account_first_name`, `account_last_name`, `account_email`, `account_phone`, `account_password`, `account_photo_path`, `account_address`) VALUES ("+ account_id +", "+ account_first_name +", "+ account_last_name +", "+ account_email +", "+ account_phone +", "+ account_password +", "+ account_photo_path +", "+ account_address +");"
+        }
+    },
+    customer: {
+        select: function() {
+            return "SELECT * FROM `goal`, business_detail, executive_profile, financial_information, main_business where main_business.business_id = financial_information.financial_information_id and main_business.business_id = executive_profile.executive_profile_id and main_business.business_id = business_detail.business_detail_id and main_business.business_id = business_detail.business_detail_id"
+        }
+    }
+}
+
+api_routes.post('/signin', function(req, res) {
+    connection.query('SELECT * FROM account WHERE username = ' + req.body.username, function (err, rows, fields) {
+        if (err) throw err
+        res.json(rows)
+
+        if (!rows.length) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' })
+        } else {
+            // check if password matches
+            const account = rows[0]
+            if (account.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' })
+            } else {
+                // create a token
+                var token = jwt.sign(account, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                })
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token.',
+                    token: token
+                })
+            }
+        }
+    })
 })
 
+// route middleware to verify a token
+api_routes.use(function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token']
+    // next()
+    // return
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+            return res.status(403).send({
+                success: false,
+                    message: 'Failed to authenticate token.'
+                })
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded
+                next()
+            }
+        })
+    } else {
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        })
+    }
+})
+
+api_routes.get('/customers', function(req, res) {
+    connection.query(query_service.customer.select(), function (err, rows, fields) {
+        if (err) throw err
+        res.json(rows)
+    })
+})
+
+api_routes.post('/customers', function(req, res) {
+    connection.query(query_service.customer.insert(), function (err, rows, fields) {
+        if (err) throw err
+        res.status(200)
+    })
+})
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 const PORT = process.env.PORT || 5000
 
-app.use('/api', apiRoutes)
+app.use('/api', api_routes)
 
 app.listen(PORT, () => {
-  console.log('Start server at port ' + PORT)
+    console.log('Start server at port ' + PORT)
 })
