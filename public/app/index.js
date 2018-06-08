caff07cb055bd71b77be762a8318b16961f3f6dd
boonprakit <boonprakit@proteus-tech.com>
2018-06-09 01:46:24 +0700

    can use file browse

--- public/app/index.js
+++ public/app/index.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ["ngRoute"])
+var app = angular.module("app", ["ngRoute", "angular-file-input"])
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
